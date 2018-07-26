36e16735fb9139a81d1ec31a0f550e79a10bd48d
ayushi-b <kanchibansal08@gmail.com>
2018-07-27 01:16:01 +0530

    Deployment changes

--- run.py
+++ run.py
@@ -1,15 +1,2 @@
 from statbot import app
 app.run(debug=True)
-
-
-'''
-/statbot
-    /statbot
-        /loaders
-            some_script.py
-            some_script2.py
-        __init__.py
-        config.py
-        views.py
-
-'''
